3549d99f1b2541bb739dadd7eeb4d2f142e458a5
Sebastian Cohnen <sebastian.cohnen@gmx.net>
2010-08-10 21:11:42 +0200

    Extended the help and also made it a class-function

--- lib/cl_console.rb
+++ lib/cl_console.rb
@@ -52,7 +52,7 @@ class CLConsole
   alias :e :escape
   
   # display help message
-  def help
+  def self.help
     puts <<-HELP_MSG
 ------------------------------------------------------------------------------
  CL-Console Help
@@ -63,7 +63,10 @@ Example usage:
 cl = CLConsole.new("db/ddoc/index")
 
 # or initialize with a full url
-cl = CLConsole.new("http://localhost:5984/db/_fti/_design/ddoc/index")    
+cl = CLConsole.new("http://localhost:5984/db/_fti/_design/ddoc/index")
+
+# you can also access couchdb-lucene direct:
+cl = CLConsole.new("http://localhost:5985/local/dewiki/_design/wiki/all")
 
 # You can also pass a hash with options to be passed to couchdb-lucene, like
 # limit, sorting, debug, ... see the couchdb-lucene documentation for details.
@@ -86,4 +89,5 @@ cl.e "~^"       # helper: proper escaping of lucene query operators
 ------------------------------------------------------------------------------
     HELP_MSG
   end
+  def help; CLConsole.help; end
 end
